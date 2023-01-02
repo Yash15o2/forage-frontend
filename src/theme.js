3a948cbc446b53c4e84d28b047ebdf51e338c7ab
Yash15o2 <yashthakur15022002@gmail.com>
2023-01-02 20:00:45 +0530

    Initial Setup

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const theme = {
+	colors: {
+		green: '#00E4A1',
+		pink: '#FF316D',
+		yellow: '#FFBE00',
+		white: '#FFFFFF',
+		black: '#000000',
+		grey: '#F5F7FA',
+	},
+	fonts: {
+		primary: '"Poppins",sans-serif',
+		secondary: '"Inter", sans-serif',
+	},
+};
+
+export default theme;
