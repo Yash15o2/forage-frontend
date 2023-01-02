84b7ea3251a07baf81191d2073256a15ff7b1408
Yash15o2 <yashthakur15022002@gmail.com>
2023-01-02 20:42:32 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<HelloWorld>Hello World</HelloWorld>
+		</div>
+	);
 }
 
+const HelloWorld = styled.p`
+	font-family: ${(props) => props.theme.fonts.primary};
+	color: ${(props) => props.theme.colors.green};
+`;
 export default App;
